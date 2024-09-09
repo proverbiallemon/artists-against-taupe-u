f5362d644a4c937be73b074a265695fddb92dc23
Robert (Lemon) Martinez-Newby <robert.mychal.martinez@gmail.com>
2024-09-08 23:25:39 -0400

    Some website functionality is implemented, styling has started to be implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import React from 'react';
+import Header from './components/header';
+import WhatWeStandFor from './components/WhatWeStandFor';
+import OurColorfulCredo from './components/OurColorfulCredo';
+import Revolutionaries from './components/Revolutionaries';
+import Partners from './components/Partners';
+import ContactForm from './components/ContactForm';
 
-const client = generateClient<Schema>();
-
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
+const App: React.FC = () => {
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div className="min-h-screen bg-background text-textColor">
+      <Header />
+      <main className="space-y-10 p-5">
+        <WhatWeStandFor />
+        <OurColorfulCredo />
+        <Revolutionaries />
+        <Partners />
+        <ContactForm />
+      </main>
+    </div>
   );
 }
 
